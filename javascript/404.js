d9fa976317a1ba5069f111686185eddba954117e
valgerte <valgertre@gmail.com>
2025-05-22 14:01:35 +0500

    c

--- javascript/404.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const bg = document.querySelector('.back404')
-  let visible = false
-
-  setInterval(() => {
-    visible = !visible
-    bg.classList.toggle('active', visible)
-  }, 3000) // каждые 3 секунды меняет состояние
-})
